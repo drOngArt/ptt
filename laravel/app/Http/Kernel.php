58d02a3f6361413b2f0f712e9a708d2102966e4f
Shift <shift@laravelshift.com>
2025-10-20 21:07:38 +0000

    Shift cleanup

--- laravel/app/Http/Kernel.php
+++ laravel/app/Http/Kernel.php
@@ -17,7 +17,7 @@ class Kernel extends HttpKernel
         'Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse',
         'Illuminate\Session\Middleware\StartSession',
         'Illuminate\View\Middleware\ShareErrorsFromSession',
-            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+        \Illuminate\Routing\Middleware\SubstituteBindings::class,
         // 'App\Http\Middleware\ExcludeCsrfTokenMiddleware',
     ];
 
